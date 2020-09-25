f47a1606033f8644009a0c38e05cee38ba090b53
Aaron <Pennwisedom@gmail.com>
2020-09-25 16:13:16 -0400

    Created Display_Pages Function

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,7 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def display_page(request, title):
+    return render(request, "encyclopedia/page.html", {
+        "entry": util.get_entry(title), "title": title
+    })
