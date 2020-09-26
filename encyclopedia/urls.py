f7eab183bcb14f7acbd191ceda4143ddb27f8771
Aaron <Pennwisedom@gmail.com>
2020-09-26 18:18:26 -0400

    Created New Page Function.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.display_page, name="display"),
-    path("search/", views.search, name="search")
+    path("search/", views.search, name="search"),
+    path("random/", views.random_page, name="random"),
+    path("newpage/", views.new_page, name="newpage")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,17 @@
 import markdown2
-from django.shortcuts import render
+import random
+
+from django.shortcuts import render, redirect
 from django import forms
 
 from . import util
 
 class NewSearchForm(forms.Form):
     searchword = forms.CharField(label="searchword")
+
+class NewPageForm(forms.Form):
+    title = forms.CharField(label="Title")
+    body = forms.CharField(label= '', widget=forms.Textarea)
 
 
 def index(request):
@@ -50,3 +56,37 @@ def search(request):
                 return render(request, "encyclopedia/search.html", { 
                     "results": results
                     })  
+
+# Goes to a Random Page
+def random_page(request):
+    pagelist = util.list_entries()
+    selected = random.choice(pagelist)
+    return redirect(display_page, title=selected)
+
+# Creates a New Page
+def new_page(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/newpage.html", {
+            "form": NewPageForm()
+        })
+
+    elif request.method == "POST":
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            body = form.cleaned_data["body"]
+            entries = []
+
+            for entry in util.list_entries():
+                entries.append(entry.lower())
+                                    
+            if title.lower() in entries:
+                error = True
+                return render(request, "encyclopedia/newpage.html", {
+                    "form": form, "error": error, "title": title 
+                })
+            
+            else:
+                util.save_entry(title, body)
+                success = True
+                return render(request, "encyclopedia/index.html", {"success": success, "title": title, "entries": util.list_entries()})
